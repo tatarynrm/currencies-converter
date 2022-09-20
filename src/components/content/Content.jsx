9c9b3e310f250d2408ee36c22a895917c92baa89
tatarynrm <tatarynrm@gmail.com>
2022-09-20 18:58:24 +0300

    all is done

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -10,11 +10,17 @@ const Content = () => {
     const [currency1, setCurrency1] = useState('USD')
     const [currency2, setCurrency2] = useState('UAH')
     const [rates, setRates] = useState([])
+    console.log(rates);
     useEffect(() => {
-        axios.get('https://api.exchangerate.host/latest?base=UAH')
-            .then(response => {
-                setRates(response.data.rates)
-            })
+        try {
+            axios.get('https://api.exchangerate.host/latest?base=UAH')
+                .then(response => {
+                    setRates(response.data.rates)
+
+                })
+        } catch (e) {
+            console.log(e);
+        }
     }, [])
 
     useEffect(() => {
@@ -22,6 +28,8 @@ const Content = () => {
             handleAmount1Change(1)
         }
     }, [rates])
+
+
 
     function format(number) {
         return number.toFixed(2)
@@ -36,7 +44,7 @@ const Content = () => {
     }
     function handleAmount2Change(amount2) {
         setAmount1(format(amount2 * rates[currency1] / rates[currency2]))
-        setAmount1(amount2)
+        setAmount2(amount2)
     }
     function handleCurrency2Change(currency2) {
         setAmount1(format(amount2 * rates[currency1] / rates[currency2]))
